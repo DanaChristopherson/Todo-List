1c6ae806e6008c6a2640718f0a1deca862a8927b
Dana Christopherson <dana.christopherson51@gmail.com>
2019-05-14 10:09:57 -0700

    Added files

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+//check off specific todos by clicking
+$("ol").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete Todo
+$("ol").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todoText from input
+      var todoText = $(this).val();
+      $(this).val("");
+        //create a new li and add to ol
+        $("ol").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle()
+});
